d7d92e1764dcf2ea39e8f1c36fff87b5b93c3048
Francis Nixon <13nixonf@gmail.com>
2018-07-23 12:41:36 -0700

    updated testing main which generates mathml.

--- src/main/java/uk/ac/ic/doc/fpn17/TestingMain.kt
+++ src/main/java/uk/ac/ic/doc/fpn17/TestingMain.kt
@@ -4,10 +4,10 @@ import uk.ac.ic.doc.fpn17.logic.*
 import uk.ac.ic.doc.fpn17.util.UUIDUtil
 
 fun main(args: Array<String>) {
-//    val forAllVar = UUIDUtil.generateUUID()
-//    val existsVar = UUIDUtil.generateUUID()
-//    val predicate = PredicateAtom({ false }, arrayOf(forAllVar, existsVar))
-//    val baseExpression = And(Or(IFF(predicate, True()), False()), Implies(True(), False()))
-//    println(Exists(ForAll(baseExpression,forAllVar),existsVar).toHtml())
-//    println((PredicateAtom({ arrayOfVariables -> false }, arrayOf(UUIDUtil.generateUUID(),UUIDUtil.generateUUID()) )).toHtml())
+    val forAllVar = VariableName()
+    val existsVar = VariableName()
+    val predicate = PredicateAtom(Predicate({ false }), arrayOf(forAllVar, existsVar))
+    val baseExpression = And(Or(IFF(predicate, True()), False()), Implies(True(), False()))
+    println(Exists(ForAll(baseExpression,forAllVar),existsVar).toHtml())
+//    println((PredicateAtom(Predicate({ arrayOfVariables -> false }), arrayOf(VariableName(),VariableName()) )).toHtml())
 }
